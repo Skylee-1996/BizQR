69570aea28bdaeda2b5c402e750228f171d58042
Sky <lhn8220@gmail.com>
2024-02-02 15:05:46 +0900

    "first"

---
+++ bizqr/src/main/java/ezen/bizqr/BizqrApplication.java
@@ -0,0 +1,13 @@
+package ezen.bizqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizqrApplication.class, args);
+	}
+
+}
