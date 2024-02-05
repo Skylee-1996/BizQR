f6353f5e0ac94c5aa82a1c96d6d39a59c99b4c35
DESKTOP-T9M088L\EZENIC-167 <lhn8220@gmail.com>
2024-02-05 15:06:50 +0900

    service 02/05

---
+++ bizqr/src/main/java/ezen/bizqr/store/controller/StoreController.java
@@ -0,0 +1,19 @@
+package ezen.bizqr.store.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@RequestMapping("/store/*")
+@Slf4j
+@Controller
+public class StoreController {
+    @GetMapping("/register")
+    public void storeRegister(){}
+
+
+}
