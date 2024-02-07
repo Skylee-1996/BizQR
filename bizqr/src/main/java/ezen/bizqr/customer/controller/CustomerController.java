a69076d491d6c669cfd56f44acea325d4756b1b2
doublecacao <sinna5351@gmail.com>
2024-02-07 14:50:09 +0900

    123

--- bizqr/src/main/java/ezen/bizqr/customer/controller/CustomerController.java
+++ bizqr/src/main/java/ezen/bizqr/customer/controller/CustomerController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CustomerController {
 
     private final CustomerService csv;
+
+    @GetMapping("/customerIndex")
+    public void index(){}
 
     @PostMapping("/basket")
     public ResponseEntity<OrderVO> basket (){
