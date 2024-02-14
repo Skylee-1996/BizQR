c93bfb0e00e995e2c102f5dacd8ba4431e4d8ea2
SadMyFace <pono2836@naver.com>
2024-02-14 12:38:10 +0900

    소셜 객체 받아오기 성공

--- bizqr/src/main/java/ezen/bizqr/user/controller/OAuthController.java
+++ bizqr/src/main/java/ezen/bizqr/user/controller/OAuthController.java
@@ -1,27 +1,27 @@
-package ezen.bizqr.user.controller;
-
-import ezen.bizqr.user.service.OAuthServiceImpl;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-@Slf4j
-@Controller
-@RequiredArgsConstructor
-@RequestMapping(value = "/login/oauth2", produces = "application/json")
-public class OAuthController {
-
-    private final PasswordEncoder passwordEncoder;
-    private final OAuthServiceImpl oAuthServiceImpl;
-
-    @GetMapping("/code/{registrationId}")
-    public String googleLogin(@RequestParam String code, @PathVariable String registrationId ){
-        log.info("테스트");
-        oAuthServiceImpl.socialLogin(code, registrationId);
-
-        return "/index";
-    }
-
-}
+//package ezen.bizqr.user.controller;
+//
+//import ezen.bizqr.user.service.OAuthServiceImpl;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Slf4j
+//@Controller
+//@RequiredArgsConstructor
+//@RequestMapping(value = "/login/oauth2", produces = "application/json")
+//public class OAuthController {
+//
+//    private final PasswordEncoder passwordEncoder;
+//    private final OAuthServiceImpl oAuthServiceImpl;
+//
+//    @GetMapping("/code/{registrationId}")
+//    public String googleLogin(@RequestParam String code, @PathVariable String registrationId ){
+//        log.info("테스트");
+//        oAuthServiceImpl.socialLogin(code, registrationId);
+//
+//        return "/index";
+//    }
+//
+//}
--- bizqr/src/main/java/ezen/bizqr/user/service/CustomOAuth2UserService.java
+++ bizqr/src/main/java/ezen/bizqr/user/service/CustomOAuth2UserService.java
@@ -1,32 +1,32 @@
-//package ezen.bizqr.user.service;
-//
-//import ezen.bizqr.user.repository.UserMapper;
-//import ezen.bizqr.user.security.UserVO;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Slf4j
-//@Service
-//public class CustomOAuth2UserService implements UserDetailsService {
-//
-//    private final UserMapper userMapper;
-//
-//    public CustomOAuth2UserService(UserMapper userMapper) {
-//        this.userMapper = userMapper;
-//    }
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        UserVO uvo = userMapper.selectEmail(username);
-//        uvo.setAuthList(userMapper.selectAuth(username));
-//
-//        log.info("login uvo >>>> {}", uvo);
-//
-//
-//
-//        return null;
-//    }
-//}
+package ezen.bizqr.user.service;
+
+import ezen.bizqr.user.repository.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        //구글로부터 받은 userRequest 데이터에 대한 후처리
+        System.out.println("getClientRegistration : " + userRequest.getClientRegistration());
+        System.out.println("getAccessToken : " + userRequest.getAccessToken().getTokenValue());
+
+        // 구글 로그인 버튼 클릭 -> 구글 로그인창 -> 로그인을 완료 -> code를 리턴(OAuth-Client 라이브러리) -> AccessToken 요청
+        // userRequest 정보 -> loadUser 함수 호출 -> 구글로부터 회원 프로필 받아준다.
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println("getAttributes : " + oAuth2User.getAttributes());
+
+        return oAuth2User;
+    }
+}
